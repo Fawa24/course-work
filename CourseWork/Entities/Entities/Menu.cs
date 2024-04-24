d8d838e874df62b2723ca47f48fe81160f698f3d
Fawa24 <ofeglol@gmail.com>
2024-04-24 07:42:46 +0300

    Connect EF and add Entities library

---
+++ CourseWork/Entities/Entities/Menu.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Entities.Entities;
+
+public partial class Menu
+{
+    public Guid MenuId { get; set; }
+
+    public string MenuName { get; set; } = null!;
+
+    public bool InStock { get; set; }
+
+    public virtual ICollection<Dish> Dishes { get; set; } = new List<Dish>();
+}
---
+++ CourseWork/Entities/Entities/UserQuestion.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Entities.Entities;
+
+public partial class UserQuestion
+{
+    public Guid QuestionId { get; set; }
+
+    public string QuestionText { get; set; } = null!;
+}
