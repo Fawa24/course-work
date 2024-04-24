19eb6afadc1c1f79c52de822c265f3b12e4a7291
Fawa24 <ofeglol@gmail.com>
2024-04-24 12:31:32 +0300

    Add DishesService

---
+++ CourseWork/Services/DishService.cs
@@ -0,0 +1,26 @@
+﻿using Entities.Entities;
+using IServiceContracts;
+using IServiceContracts.DTO;
+
+namespace Services
+{
+    public class DishService : IDishesService
+    {
+        private readonly DishDbContext _db;
+
+        public DishService(DishDbContext db)
+        {
+            _db = db;
+        }
+
+        public List<DishResponce> GetDishes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<MenuResponce> GetMenus()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
