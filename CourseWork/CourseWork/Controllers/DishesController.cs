b09057dd04d8101579c80c36af37483f8504d3a3
Fawa24 <ofeglol@gmail.com>
2024-04-25 13:28:46 +0300

    Add Controllers and Views folder

---
+++ CourseWork/CourseWork/Controllers/DishesController.cs
@@ -0,0 +1,25 @@
+﻿using IServiceContracts;
+using IServiceContracts.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseWork.Controllers
+{
+    public class DishesController : Controller
+    {
+        private readonly IDishesService _dishesService;
+
+        public DishesController(IDishesService dishesService)
+        {
+            _dishesService = dishesService;
+        }
+
+        [HttpGet]
+        [Route("/")]
+        [Route("/dishes")]
+        public IActionResult Dishes()
+        {
+            List<DishResponce> dishResponces = _dishesService.GetDishes();
+            return View(dishResponces);
+        }
+    }
+}
