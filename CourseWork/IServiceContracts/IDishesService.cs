a466d766aa80bcb5ac67003a69ba1ea31780e41b
Fawa24 <ofeglol@gmail.com>
2024-04-25 10:51:18 +0300

    Add AddDish and AddMenu methods to the DishesService and their implementation

--- CourseWork/IServiceContracts/IDishesService.cs
+++ CourseWork/IServiceContracts/IDishesService.cs
@@ -10,9 +10,21 @@ namespace IServiceContracts
         /// <returns>List of all the dishes in the form of DishResponce</returns>
         List<DishResponce> GetDishes();
         /// <summary>
+        /// Adds new dish to a Db
+        /// </summary>
+        /// <param name="request">Dish to add</param>
+        /// <returns>Dish in the form of DishResponce with newly generated DishId</returns>
+        DishResponce AddDish(DishAddRequest request);
+        /// <summary>
         /// Method for getting all the menus from the database
         /// </summary>
         /// <returns>List of all the menus in the form of MenuResponce</returns>
         List<MenuResponce> GetMenus();
+        /// <summary>
+        /// Adds new menu to a Db
+        /// </summary>
+        /// <param name="request">Menu to add</param>
+        /// <returns>Menu in the form of MenuResponce with newly generated MenuId</returns>
+        MenuResponce AddMenu(MenuAddRequest request);
     }
 }
--- CourseWork/Services/DishService.cs
+++ CourseWork/Services/DishService.cs
@@ -13,6 +13,28 @@ namespace Services
             _db = db;
         }
 
+        public DishResponce AddDish(DishAddRequest request)
+        {
+            if (request == null) throw new ArgumentNullException("Request cant be null");
+
+            Dish dishToAdd = request.ToDish();
+
+            _db.Dishes.Add(dishToAdd);
+
+            return dishToAdd.ToDishResponce();
+        }
+
+        public MenuResponce AddMenu(MenuAddRequest request)
+        {
+            if (request == null) throw new ArgumentNullException("Request cant be null");
+
+            Menu menuToAdd = request.ToMenu();
+
+            _db.Menus.Add(menuToAdd);
+
+            return menuToAdd.ToMenuResponce();
+        }
+
         public List<DishResponce> GetDishes()
         {
             throw new NotImplementedException();
