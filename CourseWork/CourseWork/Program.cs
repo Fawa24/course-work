a8144543fb09c8d9bb0cea59525c948f98d3549d
Fawa24 <ofeglol@gmail.com>
2024-04-25 12:23:59 +0300

    Add DishesService and controllers with views to main project

--- CourseWork/CourseWork/Program.cs
+++ CourseWork/CourseWork/Program.cs
@@ -1,11 +1,19 @@
 using Entities.Entities;
+using IServiceContracts;
 using Microsoft.EntityFrameworkCore;
+using Services;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<IDishesService, DishesService>();
 builder.Services.AddDbContext<DishDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 var app = builder.Build();
+
+app.UseStaticFiles();
+app.UseRouting();
+app.MapControllers();
 
 app.Run();
